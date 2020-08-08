c64302c15ec21cf2792b3fe2b67a002e02f5c4e5
SalHe <SalHe@qq.com>
2020-08-08 10:08:38 +0800

    添加服务端的商品字段

--- AntiBigDataService/src/main/java/com/salhe/summer/antibigdata/pojo/Product.java
+++ AntiBigDataService/src/main/java/com/salhe/summer/antibigdata/pojo/Product.java
@@ -1,11 +1,15 @@
 package com.salhe.summer.antibigdata.pojo;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 import org.springframework.context.annotation.Primary;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.sql.Date;
+import java.sql.Time;
+import java.sql.Timestamp;
 
 @Data
 @Entity(name = "products_db")
@@ -19,5 +23,11 @@ public class Product {
     private Float priceMax;
     @Column(name = "price_before_discount")
     private Float priceBeforeDiscount;
+    private String source;
+    private String url;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private Timestamp time;
+    private String platform;
+    private String deviceId;
 
 }
--- AntiBigDataService/src/main/java/com/salhe/summer/antibigdata/service/impl/ProductApiServiceImpl.java
+++ AntiBigDataService/src/main/java/com/salhe/summer/antibigdata/service/impl/ProductApiServiceImpl.java
@@ -31,6 +31,14 @@ public class ProductApiServiceImpl implements ProductApiService {
             return ApiResult.error().msg("价格有误");
         if (TextUtils.isEmpty(product.getName()))
             return ApiResult.error().msg("商品名称不能为空");
+        if (TextUtils.isEmpty(product.getSource()))
+            return ApiResult.error().msg("商品来源不可为空");
+        if (product.getTime() == null)
+            return ApiResult.error().msg("时间不可为空");
+        if (TextUtils.isEmpty(product.getPlatform()))
+            return ApiResult.error().msg("操作系统平台不可为空");
+        if (TextUtils.isEmpty(product.getDeviceId()))
+            return ApiResult.error().msg("设备ID不可为空");
 
         productsDao.save(product);
         return ApiResult.success().msg("添加成功").data(product);
